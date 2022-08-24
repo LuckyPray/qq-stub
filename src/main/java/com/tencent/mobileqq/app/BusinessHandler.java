2bb18273b570f5c0310a33b3f4a18cbc431c00f1
teble <xie091030@qq.com>
2022-08-24 16:33:19 +0800

    删除被混淆的方法及字段

--- src/main/java/com/tencent/mobileqq/app/BusinessHandler.java
+++ src/main/java/com/tencent/mobileqq/app/BusinessHandler.java
@@ -9,53 +9,5 @@ import java.util.List;
 
 /* compiled from: ProGuard */
 public abstract class BusinessHandler extends BaseBusinessHandler {
-    public final AppInterface a;
-    public final QQAppInterface b;
 
-    public BusinessHandler(QQAppInterface qQAppInterface) {
-        this.b = qQAppInterface;
-        this.a = qQAppInterface;
-    }
-
-    public BusinessHandler(AppInterface appInterface) {
-        if (appInterface instanceof QQAppInterface) {
-            this.b = (QQAppInterface) appInterface;
-        } else {
-            this.b = null;
-        }
-        this.a = appInterface;
-    }
-
-    public List a(int i) {
-        return this.a.getBusinessObserver(i);
-    }
-
-    public String c() {
-        return this.a.getCurrentAccountUin();
-    }
-
-    public void a(ToServiceMsg toServiceMsg) {
-        this.a.sendToService(toServiceMsg);
-    }
-
-    public final void b(ToServiceMsg toServiceMsg) {
-        throw new RuntimeException("Stub!");
-    }
-
-    public static MessageMicro a(String str, byte[] bArr, Class cls) {
-        throw new RuntimeException("Stub !");
-    }
-
-    /* renamed from: a */
-    protected final boolean m233a(ToServiceMsg toServiceMsg) {
-        throw new RuntimeException("Stub!");
-    }
-
-    protected void c(FromServiceMsg fromServiceMsg) {
-        throw new RuntimeException("Stub !");
-    }
-
-    protected boolean a(FromServiceMsg fromServiceMsg) {
-        return !fromServiceMsg.isSuccess() && (fromServiceMsg.getServiceCmd().equals("EncounterSvc.ReqGetEncounter") || fromServiceMsg.getServiceCmd().equals("RoamClientSvr.GetQualify") || fromServiceMsg.getServiceCmd().equals("NeighborSvc.ReqGetPoint"));
-    }
 }
