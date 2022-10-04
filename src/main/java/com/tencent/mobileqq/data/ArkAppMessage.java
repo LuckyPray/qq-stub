546531381ffca1cf62a44bc1d26e1fed60b9d782
ACh Sulfate <xenonhydride@gmail.com>
2022-10-04 21:24:38 +0800

    add stub class for struct msg and ark app
    Signed-off-by: ACh Sulfate <xenonhydride@gmail.com>

---
+++ src/main/java/com/tencent/mobileqq/data/ArkAppMessage.java
@@ -0,0 +1,26 @@
+package com.tencent.mobileqq.data;
+
+/**
+ * @since QQ 6.5.5, TIM 1.0.0, QQLite 4.0.1
+ */
+public class ArkAppMessage {
+
+    public final String TAG = "ArkApp.Message";
+
+    public ArkAppMessage() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public void reset() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public String toAppXml() {
+        throw new RuntimeException("Stub!");
+    }
+
+    public boolean fromAppXml(String xml) {
+        throw new RuntimeException("Stub!");
+    }
+
+}
---
+++ src/main/java/com/tencent/mobileqq/structmsg/AbsStructMsg.java
@@ -0,0 +1,16 @@
+package com.tencent.mobileqq.structmsg;
+
+import android.os.Bundle;
+import java.io.Externalizable;
+
+public abstract class AbsStructMsg implements Externalizable {
+
+    AbsStructMsg() {
+        throw new RuntimeException("Stub!");
+    }
+
+    AbsStructMsg(Bundle bundle) {
+        throw new RuntimeException("Stub!");
+    }
+
+}
