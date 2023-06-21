79373fcd84de3714c238240ebaecd17fda708525
klxiaoniu <klxiaoniu@proton.me>
2023-06-21 21:05:12 +0800

    add QUICheckBox

---
+++ src/main/java/com/tencent/mobileqq/widget/QUICheckBox.java
@@ -0,0 +1,13 @@
+package com.tencent.mobileqq.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.CheckBox;
+
+public class QUICheckBox extends CheckBox {
+
+    public QUICheckBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        throw new RuntimeException("Stub!");
+    }
+}
