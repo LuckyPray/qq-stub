5a2a136690b1f74bc2406fdca93b2b2c0078cecf
klxiaoniu <klxiaoniu@proton.me>
2023-07-06 17:01:14 +0800

    add new constructor for MsgAttributeInfo

--- src/main/java/com/tencent/qqnt/kernel/nativeinterface/MsgAttributeInfo.java
+++ src/main/java/com/tencent/qqnt/kernel/nativeinterface/MsgAttributeInfo.java
@@ -104,7 +104,27 @@ public final class MsgAttributeInfo {
         return "MsgAttributeInfo{attrType=" + this.attrType + ",attrId=" + this.attrId + ",vasMsgInfo=" + this.vasMsgInfo + ",vasPersonalInfo=" + this.vasPersonalInfo + ",groupHonor=" + this.groupHonor + ",kingHonor=" + this.kingHonor + ",publicAccountAttrs=" + this.publicAccountAttrs + ",sharedMsgInfo=" + this.sharedMsgInfo + ",gameChatSession=" + this.gameChatSession + ",uinInfoAttr=" + this.uinInfoAttr + ",}";
     }
 
+    // For QQ 8.9.63
     public MsgAttributeInfo(int i2, long j2, VASMsgElement vASMsgElement, VASAIOPersonalElement vASAIOPersonalElement, GroupHonor groupHonor, KingHonor kingHonor, PublicAccountAttrs publicAccountAttrs, SharedMsgInfo sharedMsgInfo, TempChatGameSession tempChatGameSession, UinInfoAttr uinInfoAttr,LongMsgAttr longMsgAttr) {
+        IPatchRedirector redirector = PatchRedirectCenter.getRedirector(93552, (short) 2);
+        if (redirector != null) {
+            redirector.redirect((short) 2, this, Integer.valueOf(i2), Long.valueOf(j2), vASMsgElement, vASAIOPersonalElement, groupHonor, kingHonor, publicAccountAttrs, sharedMsgInfo, tempChatGameSession, uinInfoAttr);
+            return;
+        }
+        this.attrType = i2;
+        this.attrId = j2;
+        this.vasMsgInfo = vASMsgElement;
+        this.vasPersonalInfo = vASAIOPersonalElement;
+        this.groupHonor = groupHonor;
+        this.kingHonor = kingHonor;
+        this.publicAccountAttrs = publicAccountAttrs;
+        this.sharedMsgInfo = sharedMsgInfo;
+        this.gameChatSession = tempChatGameSession;
+        this.uinInfoAttr = uinInfoAttr;
+    }
+
+    // For QQ 8.9.68+
+    public MsgAttributeInfo(int i2, long j2, VASMsgElement vASMsgElement, VASAIOPersonalElement vASAIOPersonalElement, GroupHonor groupHonor, KingHonor kingHonor, PublicAccountAttrs publicAccountAttrs, SharedMsgInfo sharedMsgInfo, TempChatGameSession tempChatGameSession, UinInfoAttr uinInfoAttr,LongMsgAttr longMsgAttr,RobotMsgExt robotMsgExt) {
         IPatchRedirector redirector = PatchRedirectCenter.getRedirector(93552, (short) 2);
         if (redirector != null) {
             redirector.redirect((short) 2, this, Integer.valueOf(i2), Long.valueOf(j2), vASMsgElement, vASAIOPersonalElement, groupHonor, kingHonor, publicAccountAttrs, sharedMsgInfo, tempChatGameSession, uinInfoAttr);
